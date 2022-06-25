0a51173633e9819a118c393a4519e422278e072c
Ene111 <89186325+Ene111@users.noreply.github.com>
2022-06-25 14:44:26 +0100

    who i am

---
+++ name.js
@@ -0,0 +1,5 @@
+let call = ' Name: Eyimoga Magdalene Enekole </br>'
+let height = 'Height: 5.6ft <br>'
+let country = 'Country: Nigeria'
+let person = `${call} ${height} ${country}`
+document.write(person)
